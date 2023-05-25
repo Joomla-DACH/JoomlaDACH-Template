bdddbd1e7ae17d82490c4beddbcbaa2c100c5ee7
nadjak77 <web@lamisch.de>
2023-05-25 08:54:12 +0200

    Überarbeitung des Accordions

--- src/templates/joomladach2023/html/mod_articles_latest/faq.php
+++ src/templates/joomladach2023/html/mod_articles_latest/faq.php
@@ -16,17 +16,22 @@ if (!$list) {
 
 ?>
 
-<div class="tabs modul__akkordion-faq">
+<div class="tabs modul__accordion-faq">
     <?php $i = 1; ?>
 <?php foreach ($list as $item) : ?>
-    <div class="modul__akkordion-faq_tab">
-        <input class="modul__akkordion-faq_radio" type="radio" id="rd<?php echo $i; ?>" name="rd">
-        <label class="modul__akkordion-faq_tab-label" for="rd<?php echo $i; ?>" itemscope itemtype="https://schema.org/Article">
-            <span itemprop="name">
+    <h3>
+    <button type="button" aria-expanded="false" class="modul__accordion-faq_trigger" aria-controls="sect<?php echo $i; ?>" id="accordion<?php echo $i; ?>id">
+      <span class="modul__accordion-faq_title">
                 <?php echo $item->title; ?>
-            </span>
-        </label>
-        <div class="modul__akkordion-faq_tab-content">
+
+      </span>
+        <svg class="modul__accordion-faq_icon" xmlns="http://www.w3.org/2000/svg" width="18.214" height="31.629" viewBox="0 0 18.214 31.629">
+            <path id="Pfad_165" data-name="Pfad 165" d="M417.213,197.147,407.7,186.863,399,197.147h5.464v21.344h7.283V197.147Z" transform="translate(417.213 218.492) rotate(180)" fill="currentColor"/>
+        </svg>
+    </button>
+    </h3>
+    <div id="sect<?php echo $i; ?>" role="region" aria-labelledby="accordion1id" class="modul__accordion-faq_panel" hidden>
+        <div>
             <?php echo $item->introtext; ?>
         </div>
     </div>
