13b72efb4d6e26672c8f4bd0f8b8019a58c85784
Lukas Jardin <l.jardin@djumla.de>
2023-08-02 09:46:49 +0200

    update markup-structure

--- src/templates/joomladach2023/html/mod_articles_latest/faq.php
+++ src/templates/joomladach2023/html/mod_articles_latest/faq.php
@@ -16,25 +16,25 @@ if (!$list) {
 
 ?>
 
-<div class="tabs modul__accordion-faq">
+<div class="accordion">
     <?php $i = 1; ?>
-<?php foreach ($list as $item) : ?>
-    <h3>
-    <button type="button" aria-expanded="false" class="modul__accordion-faq_trigger" aria-controls="sect<?php echo $i; ?>" id="accordion<?php echo $i; ?>id">
-      <span class="modul__accordion-faq_title">
-                <?php echo $item->title; ?>
-
-      </span>
-        <svg class="modul__accordion-faq_icon" xmlns="http://www.w3.org/2000/svg" width="18.214" height="31.629" viewBox="0 0 18.214 31.629">
-            <path id="Pfad_165" data-name="Pfad 165" d="M417.213,197.147,407.7,186.863,399,197.147h5.464v21.344h7.283V197.147Z" transform="translate(417.213 218.492) rotate(180)" fill="currentColor"/>
-        </svg>
-    </button>
-    </h3>
-    <div id="sect<?php echo $i; ?>" role="region" aria-labelledby="accordion1id" class="modul__accordion-faq_panel" hidden>
-        <div>
+    <?php foreach ($list as $item) : ?>
+      <div class="accordion__item">
+        <button type="button" aria-expanded="false" class="accordion__item-trigger"
+                aria-controls="sect<?php echo $i; ?>" id="accordion<?php echo $i; ?>id">
+              <span class="accordion__item-trigger-title" id="title<?php echo $i; ?>">
+                        <?php echo $item->title; ?>
+              </span>
+          <svg class="accordion__item-trigger-icon" xmlns="http://www.w3.org/2000/svg" width="18.214" height="31.629"
+               viewBox="0 0 18.214 31.629">
+            <path d="M417.213,197.147,407.7,186.863,399,197.147h5.464v21.344h7.283V197.147Z"
+                  transform="translate(417.213 218.492) rotate(180)" fill="currentColor" />
+          </svg>
+        </button>
+        <div id="sect<?php echo $i; ?>" role="region" aria-labelledby="title<?php echo $i; ?>" class="accordion__item-panel">
             <?php echo $item->introtext; ?>
         </div>
-    </div>
-    <?php $i++; ?>
-<?php endforeach; ?>
+      </div>
+      <?php $i++; ?>
+    <?php endforeach; ?>
 </div>
