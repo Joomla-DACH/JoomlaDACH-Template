faa1cec3d59ce136713c80c8b2bb478d8a35260a
Lukas Jardin <l.jardin@djumla.de>
2023-08-02 10:09:04 +0200

    fix js

--- src/templates/joomladach2023/assets/js/template.js
+++ src/templates/joomladach2023/assets/js/template.js
@@ -1,8 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
   for (let accordionItem of document.querySelectorAll('.accordion__item')) {
     accordionItem.querySelector('.accordion__item-trigger').addEventListener('click', (e) => {
-      e.target.toggleAttribute('aria-expanded')
       accordionItem.querySelector('.accordion__item-panel').classList.toggle('open');
+
+      if (e.target.getAttribute('aria-expanded') === 'true') {
+        e.target.setAttribute('aria-expanded' , 'false')
+        return;
+      }
+      e.target.setAttribute('aria-expanded' , 'true')
     })
   }
 })
