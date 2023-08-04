aef99d6518c82c5da57b26b46db22fe6180089ca
Lukas Jardin <l.jardin@djumla.de>
2023-08-04 15:49:13 +0200

    add jdachhero module and styles

---
+++ src/modules/mod_jdachhero/mod_jdachhero.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @version    CVS: 1.0.0
+ * @package    Mod_HomepageHero
+ * @author     djumla GmbH, Lukas Jardin <info@djumla.de>
+ * @copyright  2019 djumla GmbH
+ * @license    GNU General Public License Version 2 oder später; siehe LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+$document = Factory::getDocument();
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8');
+require JModuleHelper::getLayoutPath('mod_jdachhero', $params->get('layout', 'default'));
