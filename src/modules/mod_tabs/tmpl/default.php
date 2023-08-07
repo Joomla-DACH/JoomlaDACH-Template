630a7f32425a1ce01b63cbdb0db79ef3df4d56e7
Lukas Jardin <l.jardin@djumla.de>
2023-08-07 17:12:32 +0200

    add unique identifier for accordion and tabs

--- src/modules/mod_tabs/tmpl/default.php
+++ src/modules/mod_tabs/tmpl/default.php
@@ -19,6 +19,8 @@ $wa = $doc->getWebAssetManager();
 $wa->registerAndUseScript('tabs', 'media/mod_tabs/js/mod_tabs.js', [], ['defer' => true], []);
 
 PluginHelper::importPlugin('content');
+
+$randomId = substr(md5(uniqid()), 0, 12);
 ?>
 
 
@@ -26,7 +28,7 @@ PluginHelper::importPlugin('content');
   <div role="tablist" class="automatic tabs__tablist">
     <?php $navindex = 1; ?>
     <?php foreach($params->get('items') as $item): ?>
-      <button id="tab-<?php echo $navindex; ?>" type="button" role="tab" aria-selected="<?php echo $navindex === 1 ? 'true' : 'false' ;?>" aria-controls="tabpanel-<?php echo $navindex; ?>" <?php echo $navindex > 1 ? 'tabindex="-1"' : '' ; ?>>
+      <button id="tab-<?php echo $navindex; ?>" type="button" role="tab" aria-selected="<?php echo $navindex === 1 ? 'true' : 'false' ;?>" aria-controls="tabpanel-<?php echo $randomId . $navindex; ?>" <?php echo $navindex > 1 ? 'tabindex="-1"' : '' ; ?>>
         <?php echo $item->item_title; ?>
       </button>
       <?php $navindex++; ?>
@@ -35,7 +37,7 @@ PluginHelper::importPlugin('content');
   <div class="tabs__panels">
     <?php $panelindex = 1; ?>
       <?php foreach($params->get('items') as $item): ?>
-        <div id="tabpanel-<?php echo $panelindex; ?>" role="tabpanel" tabindex="0" aria-labelledby="tab-<?php echo $panelindex; ?>">
+        <div id="tabpanel-<?php echo $randomId . $panelindex; ?>" role="tabpanel" tabindex="0" aria-labelledby="tab-<?php echo $panelindex; ?>">
             <?php
             $content = HTMLHelper::_('content.prepare', $item->item_content, '', 'mod_tabs.content');
             echo $content;
