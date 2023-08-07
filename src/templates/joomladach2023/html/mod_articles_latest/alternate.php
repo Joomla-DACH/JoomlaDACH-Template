0a5d5aadf294bd089575c6b84188894a312a28b5
Lukas Jardin <l.jardin@djumla.de>
2023-08-07 13:31:44 +0200

    add styles and structured for alternating layouts
    mod_articles_latest
    
    - Default (no readmore, no date, introimage)
    - Counter (Default + Counter-Elements)
    - News (Including Readmorelink and Publishing-Date, hiding introimage)

---
+++ src/templates/joomladach2023/html/mod_articles_latest/alternate.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_articles_latest
+ *
+ * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+if (!$list) {
+    return;
+}
+
+?>
+
+<div class="mod-articles-latest-alternate">
+    <?php foreach ($list as $item) : ?>
+      <div class="mod-articles-latest-alternate__item">
+        <div class="mod-articles-latest-alternate__item-content">
+          <h3 class="h2 mod-articles-latest-alternate__item-content-title"><?php echo $item->title; ?></h3>
+          <div class="mod-articles-latest-alternate__item-content-introtext">
+              <?php echo $item->introtext; ?>
+          </div>
+        </div>
+          <?php if(!empty(json_decode($item->images)->image_intro)): ?>
+              <?php
+              $sizes = getimagesize(explode('#',json_decode($item->images)->image_intro)[0]);
+              ?>
+            <img
+              src="<?php echo explode('#',json_decode($item->images)->image_intro)[0];?>"
+              loading="lazy"
+              alt="<?php echo $item->title; ?>"
+              class="mod-articles-latest-alternate__item-image"
+                <?php if ($sizes): ?>
+                  width="<?php echo $sizes[0]; ?>" height="<?php echo $sizes[1]; ?>"
+                <?php endif; ?>
+            />
+          <?php endif; ?>
+      </div>
+    <?php endforeach; ?>
+</div>
