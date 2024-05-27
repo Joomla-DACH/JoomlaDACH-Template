4fff936635905fdbd236faf0ee11987a58671de2
David Jardin <d.jardin@djumla.de>
2024-05-27 08:47:42 +0200

    ADded module

---
+++ src/plugins/content/loadrandommodule/services/provider.php
@@ -0,0 +1,38 @@
+<?php
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use JoomlaDACH\Plugin\Content\LoadRandomModule\Extension\LoadRandomModule;
+
+return new class () implements ServiceProviderInterface {
+    /**
+     * Registers the service provider with a DI container.
+     *
+     * @param   Container  $container  The DI container.
+     *
+     * @return  void
+     *
+     * @since   4.4.0
+     */
+    public function register(Container $container): void
+    {
+        $container->set(
+            PluginInterface::class,
+            function (Container $container) {
+                $plugin     = new LoadRandomModule(
+                    $container->get(DispatcherInterface::class),
+                    (array) PluginHelper::getPlugin('content', 'loadrandommodule')
+                );
+                $plugin->setApplication(Factory::getApplication());
+
+                return $plugin;
+            }
+        );
+    }
+};
