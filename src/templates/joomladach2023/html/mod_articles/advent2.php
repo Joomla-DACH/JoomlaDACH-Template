081ad39b4945a4e45998b0ccaf5bdd6da8e6a253
Stefan Wendhausen <stefan.wendhausen@tec-promotion.de>
2024-11-18 11:48:33 +0100

    remove iframe

--- src/templates/joomladach2023/html/mod_articles/advent2.php
+++ src/templates/joomladach2023/html/mod_articles/advent2.php
@@ -14,22 +14,12 @@ use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
 use Joomla\CMS\Router\Route;
 
-/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
-$wa = $app->getDocument()->getWebAssetManager();
-$wa->useScript('joomla.dialog-autocreate');
-
 if (!$list) {
 	return;
 }
 
 $items = $list;
 $today = Factory::getDate()->format('Y-m-d');
-
-// Set up the modal options that will be used for module editor
-$popupOptions = [
-	'popupType'  => 'iframe',
-	'className'  => 'adventskalender',
-];
 ?>
 
 <div class="advent-grid advent-bg">
@@ -59,14 +49,11 @@ $popupOptions = [
 
         <div class="mod-articles-advent-item-content">
 			<?php if ($adventDate && $adventDate <= $today) : ?>
-                <button type="button"
-                        data-joomla-dialog="<?php echo htmlspecialchars(json_encode($popupOptions, JSON_UNESCAPED_SLASHES)) ?>"
-                        class="btn btn-link advent-link"
-                        aria-label="Öffnet <?php echo $item->title; ?>"
-                        id="title-<?php echo $item->id; ?>"
-                        data-module-id="<?php echo $item->id; ?>">
+                <a href="<?php echo Route::_('index.php?view=article&id=' . $item->id); ?>"
+                        class="advent-link"
+                        aria-label="Öffnet <?php echo $item->title; ?>">
                     <span><?php echo date('j', strtotime($adventDate)); ?></span>
-                </button>
+                </a>
 			<?php else : ?>
                 <span><?php echo date('j', strtotime($adventDate)); ?></span>
 			<?php endif; ?>
